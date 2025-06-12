01ffe014a6883e2dc2e7c709e57f95f1f17bdaac
IsisNarvaez <isis.narvaez@upr.edu>
2025-06-12 13:43:56 -0400

    make data: code 3 datasets

--- data-raw/make-data.R
+++ data-raw/make-data.R
@@ -1,18 +1,61 @@
 # data-raw/make-data.R
 
-if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-BiocManager::install(c("ExperimentHubData", "AnnotationHubData"))
+# Load the package
+library(iModMix)
 
-library(ExperimentHubData)
-library(AnnotationHubData)
+# PartialCorGenes
 
-pkg_dir <- normalizePath("../iModMixData")
+# Get the path to the expression data file
+path_gene_exp <- system.file("Example_data/ccRCC4_Data/RNA_exp.rds", package = "iModMix")
 
-metadata_path <- file.path(pkg_dir, "inst/extdata/metadata.csv")
-stopifnot(file.exists(metadata_path))
+# Load the expression data
+Data_exp1 <- readRDS(path_gene_exp)
 
-makeExperimentHubMetadata(pkgname = "iModMixData", file = metadata_path)
+# Preprocess of data for iModMix upload
+load_data1 <- load_data(Data_exp1)
 
-ExperimentHubData::uploadResources(pkgname = "iModMixData")
+# Perform Partial correlation
+PartialCorGenes <- partial_cors(load_data1, rho = 0.25)
 
-cat("Data ready to ExperimentHub.\n")
+# save in inst/extdata
+saveRDS(PartialCorGenes, file = "inst/extdata/ccRCC4_Data/PartialCorGenes.rds")
+
+
+
+# PartialCorMetabolites
+
+# Get the path to the expression data file
+path_Metab_exp <- system.file("Example_data/ccRCC4_Data/Metab_exp.rds", package = "iModMix")
+
+# Load the expression data
+Data_exp2 <- readRDS(path_Metab_exp)
+
+# Preprocess of data for iModMix upload
+load_data2 <- load_data(Data_exp2)
+
+# Perform Partial correlation
+PartialCorMetabolites <- partial_cors(load_data2, rho = 0.25)
+
+# save in inst/extdata
+saveRDS(PartialCorMetabolites, file = "inst/extdata/ccRCC4_Data/PartialCorMetabolites.rds")
+
+
+
+# PartialCorProt
+
+# Get the path to the expression data file
+path_Prot_exp <- system.file("Example_data/FloresData_K_TK/Metab_exp.rds", package = "iModMix")
+
+# Load the expression data
+Data_exp3 <- readRDS(path_Prot_exp)
+
+# Preprocess of data for iModMix upload
+load_data3 <- load_data(Data_exp3)
+
+# Perform Partial correlation
+PartialCorProt <- partial_cors(load_data3, rho = 0.25)
+
+# save in inst/extdata
+saveRDS(PartialCorProt, file = "inst/extdata/FloresData_K_TK/PartialCorProt.rds")
+
+
